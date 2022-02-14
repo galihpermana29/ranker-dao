2f6f55e99d0548b78bd52e5217b289a937b61eaa
Philip Arthur Moore <philip@hotcross.com>
2022-02-14 16:43:03 +0700

    Add in badge mint functionality.

--- src/pages/mintbadge/index.js
+++ src/pages/mintbadge/index.js
@@ -1,57 +1,203 @@
 import './style.scss';
+import {useState, useEffect} from 'react';
 import ShieldBadge from '../../assets/img/shield-with-shadow.png';
+import {connectWallet, getAccounts, addNetwork} from '../../services/provider';
+import {mint, getAccountBalance} from '../../services/mint';
+
 const MintBadge = () => {
+  const [txHash, setTxHash] = useState('')
+  const [account, setAccount] = useState()
+  const [balance, setBalance] = useState(-1)
+  const [chainId, setChainId] = useState()
+  const [loading, setLoading] = useState(false)
+
+  const onConnect = async () => {
+    const provider = await connectWallet()
+    setChainId(Number(provider.chainId))
+    const account = await getAccounts()
+    setAccount(account)
+
+    provider.on("accountsChanged", (accounts) => {
+      setAccount(accounts[0])
+    });
+
+    provider.on("chainChanged", (chain) => {
+      setChainId(Number(chain))
+    });
+  }
+
+  const onDisconnect = () => {
+    setAccount()
+  }
+
+  const updateAccountBalance = async () => {
+    const accountBalance = await getAccountBalance(account)
+
+    setBalance(accountBalance.toString())
+  }
+
+  const onMint = async () => {
+    setLoading(true)
+    const tx = await mint();
+
+    setTxHash(tx.hash)
+    updateAccountBalance()
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if(account) {
+      updateAccountBalance()
+    }
+    else {
+      setTxHash('')
+    }
+  }, [account])
+
+  useEffect(() => {
+    if(chainId && Number(chainId) !== Number(process.env.REACT_APP_SUPPORTED_CHAIN_ID)) {
+      addNetwork()
+    }
+  }, [chainId])
+
+  const onSwitchNetwork = () => {
+    addNetwork()
+  }
+
+  const renderError = () => chainId && Number(chainId) !== Number(process.env.REACT_APP_SUPPORTED_CHAIN_ID) ? (
+    <a onClick={onSwitchNetwork} className="link">
+      <p className="bold-15 text-white my-4 d-none d-lg-block pad-right error">
+        {`Please connect to ${process.env.REACT_APP_NETWORK_NAME}`}
+      </p>
+    </a>
+  ) : null
+
+  const isButtonDisabled = () => {
+    return (chainId && Number(chainId) !== Number(process.env.REACT_APP_SUPPORTED_CHAIN_ID)) || Number(balance) > 0 || !account || loading || txHash
+  }
+
+  const getButtonClass = () => {
+    if(isButtonDisabled()) {
+      return 'mint-btn-disabled'
+    }
+
+    return 'mint-btn'
+  }
+
+  const formatAccount = account => `${account.slice(0, 6)}...${account.slice(account.length - 6, account.length)}`
+
   return (
     <div className="container-fluid p-0">
       <div className="row p-0 m-0 hero-hero gx-5">
         <div className="col-sm-12 col-lg-6 py-4 px-5 p-lg-5 d-flex flex-column align-items-center align-items-lg-start justify-content-start mt-3 mt-lg-5">
-          <p className="akira-outlined-white">MINT YOUR</p>
-          <p className="akira-fill-white">
-            GUILD{' '}
-            <span className="akira-fill-white yellow-half-bottom">BADGE</span>
-          </p>
-          <p className="bold-15 text-white my-4 d-none d-lg-block pad-right">
-            You can enjoy our inventory of NFTs and other boons with your{' '}
-            <span className="bold-white">RANKER Badge</span>. Exclusive NFTs,
-            Tournaments, coaching sessions and beyond.
-          </p>
-          <p className="bold-15 text-white my-4 d-block d-lg-none px-5">
-            You can enjoy our inventory of NFTs and other boons with your{' '}
-            <span className="bold-white">RANKER Badge</span>. Exclusive NFTs,
-            Tournaments, coaching sessions and beyond.
-          </p>
-          <div className="d-flex flex-row">
-            <div className="px-0 d-none d-lg-block">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <button className="mint-btn my-4 bold-white">
-                  MINT YOUR BADGE
-                </button>
-              </a>
-            </div>
-            <div className="px-4 d-none d-lg-block">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <button className="connect-btn my-4 text-blue">
-                  CONNECT WALLET
-                </button>
-              </a>
-            </div>
-          </div>
-          <div className="row d-block d-lg-none">
-            <div className="col-sm-12 col-lg-6">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <button className="mint-btn my-4 bold-white">
-                  MINT YOUR BADGE
-                </button>
-              </a>
-            </div>
-            <div className="col-sm-12 col-lg-6">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <button className="connect-btn my-4 text-blue">
-                  CONNECT WALLET
-                </button>
-              </a>
-            </div>
-          </div>
+        <p className="akira-outlined-white">
+                MINT YOUR 
+              </p>
+              <p className="akira-fill-white">GUILD BADGE</p>
+              <p className="bold-15 text-white my-4 d-none d-lg-block pad-right">
+              You can enjoy our inventory of NFTs and other boons with your <span className="bold-white">RANKER Badge</span>. Exclusive NFTs, Tournaments, 
+              coaching sessions and beyond. Mint your RGUILD basic beta badge to become part of the RankerDAO guild today.
+              </p>
+              {account ? (<p className="bold-15 text-white my-4 d-none d-lg-block pad-right">
+                  You are currently connected with <a
+                    className="link"
+                    href={`${process.env.REACT_APP_EXPLORER_URL}/address/${account}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >{formatAccount(account)}</a>
+                </p>
+              ) : null}
+              <p className="bold-15 text-white my-4 d-block d-lg-none px-5">
+              You can enjoy our inventory of NFTs and other boons with your <span className="bold-white">RANKER Badge</span>. Exclusive NFTs, Tournaments, 
+              coaching sessions and beyond. Mint your RGUILD basic beta badge to become part of the RankerDAO guild today.
+              </p>
+              {account ? (
+                <p className="bold-15 text-white my-4 d-block d-lg-none px-5">
+                  You are currently connected with <a
+                    className="link"
+                    href={`${process.env.REACT_APP_EXPLORER_URL}/address/${account}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >{formatAccount(account)}</a>
+                </p>
+              ) : null}
+              <div className="d-flex flex-row">
+                {account ? (
+                  <div className="px-0 d-none d-lg-block">
+                    <button onClick={onDisconnect} className="connect-btn my-4 text-blue">DISCONNECT WALLET</button>
+                  </div>
+                ) : (
+                  <div className="px-0 d-none d-lg-block">
+                    <button onClick={onConnect} className="connect-btn my-4 text-blue">CONNECT WALLET</button>
+                  </div>
+                )}
+                <div className="px-4 d-none d-lg-block">
+                  <button onClick={isButtonDisabled() ? () => {} : onMint} className={`${getButtonClass()} my-4 bold-white`}>MINT YOUR BADGE</button>
+                </div>
+              </div>
+              <div className="row d-block d-lg-none">
+                {account ? (
+                  <div className="col-sm-12 col-lg-6">
+                    <button onClick={onDisconnect} className="connect-btn my-4 text-blue">DISCONNECT WALLET</button>
+                  </div>
+                ) : (
+                  <div className="col-sm-12 col-lg-6">
+                    <button onClick={onConnect} className="connect-btn my-4 text-blue">CONNECT WALLET</button>
+                  </div>
+                )}
+                <div className="col-sm-12 col-lg-6">
+                  <button onClick={isButtonDisabled() ? () => {} : onMint} className={`${getButtonClass()} my-4 bold-white`}>MINT YOUR BADGE</button>
+                </div>
+              </div>
+              {txHash ? (
+                <a
+                  className="link"
+                  href={`${process.env.REACT_APP_EXPLORER_URL}/tx/${txHash}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="bold-15 text-white my-4 d-none d-lg-block pad-right">
+                    View your transaction on BscScan
+                  </p>
+                </a>
+              ) : null}
+              {txHash ? (
+                <a
+                  className="link"
+                  href={`${process.env.REACT_APP_EXPLORER_URL}/tx/${txHash}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="bold-15 text-white my-4 d-block d-lg-none px-5">
+                    View your transaction on BscScan
+                  </p>
+                </a>
+              ) : null}
+              {Number(balance) > 0 && account ? (
+                <a
+                  className="link"
+                  href={`${process.env.REACT_APP_EXPLORER_URL}/token/${process.env.REACT_APP_NFT_ADDRESS}?a=${account}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="bold-15 text-white my-4 d-none d-lg-block pad-right">
+                    You have already minted your badge
+                  </p>
+                </a>
+              ) : null}
+              {Number(balance) > 0 && account ? (
+                <a
+                  className="link"
+                  href={`${process.env.REACT_APP_EXPLORER_URL}/token/${process.env.REACT_APP_NFT_ADDRESS}?a=${account}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <p className="bold-15 text-white my-4 d-block d-lg-none px-5">
+                    You have already minted your badge
+                  </p>
+                </a>
+              ) : null}
+              {renderError()}
         </div>
 
         <div className="col-sm-12 col-lg-6 pb-5 p-lg-5">
---
+++ src/services/mint.js
@@ -0,0 +1,29 @@
+import {ethers} from 'ethers';
+import ERC721Abi from './ERC721.json';
+import {getProvider} from './provider';
+
+export const mint = async () => {
+  const provider = getProvider();
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    process.env.REACT_APP_NFT_ADDRESS,
+    ERC721Abi,
+    signer
+  );
+
+  const tx = await contract.mint();
+
+  return tx;
+}
+
+export const getAccountBalance = async account => {
+  const provider = getProvider();
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    process.env.REACT_APP_NFT_ADDRESS,
+    ERC721Abi,
+    signer
+  );
+
+  return await contract.balanceOf(account);
+}
