55c0ca413934a7b8b09feb99b4086ca3bce0f68d
arian <arian@MacBook-Pro.local>
2022-09-05 13:01:16 +0700

    Change all screen font size

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,10 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Modal } from 'bootstrap';
 import 'animate.css';
 
-// import { Footer } from '../../components';
 import { ModalFriends, Sidebar } from 'components';
-
-import './style.scss';
 import { FirstSection } from './FirstSection';
 import { SecondSection } from './SecondSection';
 import { ThirdSection } from './ThirdSection';
@@ -14,20 +11,13 @@ import { FourthSection } from './FourthSection';
 import { FifthSection } from './FifthSection';
 import { SixthSection } from './SixthSection';
 
+import './style.scss';
+
 const Home = () => {
   const [activeSection, setActiveSection] = useState('home-first-section');
   const [isOpenMintingSites, setIsOpenMintingSites] = useState(false);
 
   const modalRef = useRef(null);
-
-  // const SectionsId = [
-  //   'home-first-section',
-  //   'home-second-section',
-  //   'home-third-section',
-  //   'home-fourth-section',
-  //   'home-fifth-section',
-  //   'home-sixth-section',
-  // ];
 
   const showFriendsModal = () => {
     const modalEle = modalRef.current;
@@ -51,55 +41,6 @@ const Home = () => {
     };
     showModal();
   }, [activeSection]);
-
-  // Couldnt create a reusable/map the component, bug with react-scroll
-  // const RenderSection = useCallback(
-  //   ({ id = '', bgImageDesktop, bgImageMobile, heroImage, children }) => {
-  //     return (
-  //       <section
-  //         className={`home-section animate__animated ${
-  //           activeSection === id ? 'animate__fadeIn' : 'animate__fadeOut'
-  //         }`}
-  //         id={id}>
-  //         {/* Mobile */}
-  //         <img
-  //           src={bgImageMobile}
-  //           alt="background section"
-  //           className=" home-section-background d-block d-sm-none"
-  //         />
-  //         {/* Desktop */}
-  //         <img
-  //           src={bgImageDesktop}
-  //           alt="background section"
-  //           className=" home-section-background d-none d-sm-block"
-  //         />
-  //         {children}
-  //         <div className="home-section-right">
-  //           <img
-  //             src={heroImage}
-  //             alt="get set play earn"
-  //             className="home-section-hero-image"
-  //           />
-  //         </div>
-  //       </section>
-  //     );
-  //   },
-  //   [activeSection],
-  // );
-
-  // const animationFadeInClass = id => {
-  //   if (id === activeSection) {
-  //     return 'animate__fadeIn';
-  //   }
-  //   return 'animate__fadeOut';
-  // };
-
-  // const animationSlideClass = id => {
-  //   if (id === activeSection) {
-  //     return 'animate__fadeInLeft';
-  //   }
-  //   return 'animate__fadeOut';
-  // };
 
   return (
     <div className="container-fluid p-0 m-0 home-section-parent" id="home">
